d3ad7c60b4b0f2b9c5bf416b1a7876746474dcc6
linkserendipity <462710683@qq.com>
2021-02-23 13:21:33 +0800

    st

---
+++ do.sh
@@ -0,0 +1,3 @@
+python3 train_market.py --PCB --gpu_ids 2 --name ft_ResNet50_pcb_market_e --erasing_p 0.5 --train_all --data_dir "/home/huangpg/st-reid/dataset/market_rename/"
+
+# python3 gen_st_model_market.py --name ft_ResNet50_pcb_market_e --data_dir "/home/huangpg/st-reid/dataset/market_rename/"
